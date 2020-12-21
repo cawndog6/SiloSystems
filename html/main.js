22fbda5adadd5416f5fa4f0eceeb7ce8df7d3a27
lucasthoms <60754782+lucasthoms@users.noreply.github.com>
2020-12-21 11:34:21 -0600

    Added frontend support for FireBase authentication

--- html/main.js
+++ html/main.js
@@ -1,15 +1,11 @@
-//################## INITIALIZATION #######################
+//################# INITIALIZATION ########################
 
 const level = {DEBUG: 0, WARNING: 1, ERROR: 2};
 let logLevel = level.DEBUG;
 
-let loggedIn = false;
-
 let apiRoot = "https://us-west2-silo-systems-292622.cloudfunctions.net";
 
-window.addEventListener("load", StartLoad);
-
-//################## UTILITY FUNCTIONS ####################
+//################# COMMON FUNCTIONS #####################
 
 function Log(thisLevel, msg) {
     if(thisLevel >= logLevel) {
@@ -35,14 +31,36 @@ async function Get(url, isJson) {
     let response = await fetch(url);
     
     if(response.ok) {
-        Log(level.DEBUG, `Request to ${url} succeeded with code ${response.status}`);
+        Log(level.DEBUG, `Get request to ${url} succeeded with code ${response.status}`);
         if(isJson) {
             return await response.json();
         } else {
             return await response.text();
         }
     } else {
-        Log(level.ERROR, `Request to ${url} failed with code ${response.status}`);
+        Log(level.ERROR, `Get request to ${url} failed with code ${response.status}`);
+        return undefined;
+    }
+}
+
+async function Post(url, body, isJson) {
+    if(isJson === undefined) {
+        isJson = true;
+    }
+
+    let init = {method: "POST", body: body} 
+
+    let response = await fetch(url, init);
+    
+    if(response.ok) {
+        Log(level.DEBUG, `Post request to ${url} succeeded with code ${response.status}`);
+        if(isJson) {
+            return await response.json();
+        } else {
+            return await response.text();
+        }
+    } else {
+        Log(level.ERROR, `Post request to ${url} failed with code ${response.status}`);
         return undefined;
     }
 }
@@ -96,13 +114,6 @@ function DrawLineChart(dataTable, x_title, y_title, width, height, vAxis_fmt) {
 
 //################# DATA FUNCTIONS ########################
 
-function StartLoad() {
-    ValidateUser();
-
-    Promise.all([google.charts.load('current', {'packages':['corechart']}),Get("shim_sensors")])
-        .then(DrawSensorTable);
-}
-
 function DrawSensorTable(response) {
     let sensorTable = document.getElementById("sensor-table").tBodies[0];
 
@@ -144,7 +155,7 @@ function DrawSensorTable(response) {
 }
 
 function ViewSensor(event) {
-    if(!loggedIn) {
+    if(token === undefined) {
         alert("Sorry, you must be logged in to perform that action. Please log in and try again.");
         return;
     }
@@ -158,7 +169,7 @@ function ViewSensor(event) {
 }
 
 function DownloadSensor(event) {
-    if(!loggedIn) {
+    if(token === undefined) {
         alert("Sorry, you must be logged in to perform that action. Please log in and try again.");
         return;
     }
@@ -179,71 +190,4 @@ function DownloadSensor(event) {
         dummyLink.click();
         dummyLink.remove();
     });
-}
-
-//################# LOGIN FUNCTIONS #######################
-
-function ShowLogin() {
-    let template = document.getElementsByTagName("template")[0];
-    let login = template.content.getElementById("login").cloneNode(true);
-    document.body.appendChild(login);
-}
-
-function UnshowLogin() {
-    document.body.classList.remove("noScroll");
-    let login = document.getElementById("login");
-    login.remove();
-}
-
-function ValidateLogin() {
-    let form = document.getElementById("login-form");
-
-    let username = form.elements.username.value;
-    let password = form.elements.password.value;
-    let remember = form.elements.remember.checked;
-
-    if(username == password) {
-        Log(level.DEBUG, `Successful ${(remember) ? "persistent":"session"} login for user ${username}`);
-        let today = new Date();
-        let todayPlus30 = new Date();
-        todayPlus30.setDate(today.getDate()+30);
-
-        if(remember) {
-            document.cookie = `username=${username}; expires=${todayPlus30}`;
-        } else {
-            document.cookie = `username=${username}`;
-        }
-
-        ValidateUser();
-        UnshowLogin();
-    } else {
-        Log(level.WARNING, `Invalid login attempt for user ${username}`);
-        document.getElementById("login-failure").classList.remove("hidden");
-    }
-}
-
-function Logout() {
-    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    window.location.reload();
-}
-
-function ValidateUser() {
-    let userDisplay = document.getElementById("current-user");
-    let loginLink = document.getElementById("show-login-link");
-    let logoutLink = document.getElementById("logout-link");
-
-    if(document.cookie.indexOf("username=") == -1) {
-        userDisplay.innerText = "You are not logged in. ";
-        loginLink.classList.remove("hidden");
-        logoutLink.classList.add("hidden");
-
-        loggedIn = false;
-    } else {
-        let username = document.cookie.split("username=")[1].split(";")[0];
-        userDisplay.innerHTML = `Currently logged in as <strong>${username}</strong>`;
-        loginLink.classList.add("hidden");
-        logoutLink.classList.remove("hidden");
-
-        loggedIn = true;
-    }
 }
