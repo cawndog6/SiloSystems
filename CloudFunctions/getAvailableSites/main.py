e7bb5db42d9f4c464a3cfba95ce40fb7812fb2fb
Connor Williams <47013360+cawndog6@users.noreply.github.com>
2021-01-08 19:40:50 -0800

    debugging

--- CloudFunctions/getAvailableSites/main.py
+++ CloudFunctions/getAvailableSites/main.py
@@ -46,7 +46,7 @@ def getAvailableSites(request):
    #execute sql statements
    with pool.connect() as conn:
       #check requestor_uid is authenticated as site owner to add new user
-      results = conn.execute(sqlalchemy.text("SELECT site_id, role_id FROM site_user_role WHERE site_user_role.uid = "{}";".format(uid)))
+      results = conn.execute(sqlalchemy.text("SELECT site_id, role_id FROM site_user_role WHERE site_user_role.uid = '{}';".format(uid)))
       #numRows = len(results._saved_cursor._result.rows)
       #if numRows < 1:
          #return ('', 404, {'Access-Control-Allow_Origin':'*'})
