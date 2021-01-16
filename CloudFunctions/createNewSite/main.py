9ea0485e6f9f5000c46e39cf5605c9ad8948c95c
cawndog6 <47013360+cawndog6@users.noreply.github.com>
2021-01-16 13:46:37 -0800

    fixed indentation error

--- CloudFunctions/createNewSite/main.py
+++ CloudFunctions/createNewSite/main.py
@@ -19,15 +19,15 @@ def createNewSite(request):
 
    #get arguments to http request
    request_args = request.args
-   if request_args and 'site_name' in request_args:
-       site_name = request_args['site_name']
+    if request_args and 'site_name' in request_args:
+        site_name = request_args['site_name']
     if request_args and 'uid' in request_args:
         uid = request_args['uid']
 
    #connect to the database
    pool = sqlalchemy.create_engine(
-      # Equivalent URL:
-      #mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
+        # Equivalent URL:
+        #mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
         sqlalchemy.engine.url.URL(
         drivername="mysql+pymysql",
         username=db_user,  # e.g. "my-database-user"
@@ -43,22 +43,22 @@ def createNewSite(request):
     
     #execute sql statements
    with pool.connect() as conn:
-      #Add new site in database
-      #Check if site already exists for the uid (owner)
-      result = conn.execute(sqlalchemy.text("SELECT * FROM site INNER JOIN site_user_role on site.site_id = site_user_role.site_id WHERE site.site_name = '{}' AND site_user_role.uid = '{}';".format(site_name, uid)))
-      if int(result.rowcount) != 0:
-         return('Site already exists for this user.', 500, {'Access-Control-Allow-Origin':'*'})
-      else:
-         #create new entry for site in site table 
-         conn.execute(sqlalchemy.text("INSERT INTO site(site_name) VALUES ('{}');".format(site_name)))
-         #get the new site's site_id
-         result = conn.execute(sqlalchemy.text("SELECT site.site_id FROM site WHERE site.site_name = '{}';".format(site_name)))
-         greatest_id = 0
-         for r in result:
-            if int(r[0]) > greatest_id:
-               greatest_id = int(r[0])
-         site_id = greatest_id
-         #add user to site_user_role as the site owner
-         role_id = 0
-         conn.execute(sqlalchemy.text("INSERT INTO site_user_role VALUES ({}, '{}', {});".format(int(site_id), uid, role_id)))
-         return ('', 201, {'Access-Control-Allow-Origin':'*'})
+        #Add new site in database
+        #Check if site already exists for the uid (owner)
+        result = conn.execute(sqlalchemy.text("SELECT * FROM site INNER JOIN site_user_role on site.site_id = site_user_role.site_id WHERE site.site_name = '{}' AND site_user_role.uid = '{}';".format(site_name, uid)))
+        if int(result.rowcount) != 0:
+            return('Site already exists for this user.', 500, {'Access-Control-Allow-Origin':'*'})
+        else:
+            #create new entry for site in site table 
+            conn.execute(sqlalchemy.text("INSERT INTO site(site_name) VALUES ('{}');".format(site_name)))
+            #get the new site's site_id
+            result = conn.execute(sqlalchemy.text("SELECT site.site_id FROM site WHERE site.site_name = '{}';".format(site_name)))
+            greatest_id = 0
+            for r in result:
+                if int(r[0]) > greatest_id:
+                    greatest_id = int(r[0])
+                    site_id = greatest_id
+            #add user to site_user_role as the site owner
+            role_id = 0
+            conn.execute(sqlalchemy.text("INSERT INTO site_user_role VALUES ({}, '{}', {});".format(int(site_id), uid, role_id)))
+            return ('', 201, {'Access-Control-Allow-Origin':'*'})
