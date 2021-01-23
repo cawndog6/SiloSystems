6e8cc838d8d0f94d6aadf861b3250502159144d4
cawndog6 <will5828@vandals.uidaho.edu>
2021-01-22 18:46:06 -0800

    Finished removeDeviceFromSite, removeUserFromSiteadded new getSiteDeviceInformation function, made changes to some header comments

--- CloudFunctions/addDeviceToSite/main.py
+++ CloudFunctions/addDeviceToSite/main.py
@@ -1,7 +1,7 @@
 #Author(s): Connor Williams
 #Date: 1/21/2021
 #Purpose: Take in arguments from an HTTP request for uid, site_id, and device_name and run sql queries to add the the device to the site's database
-#Trigger: https://us-west2-silo-systems-292622.cloudfunctions.net/createNewSite?site_name=newSiteName&uid=abcdabcd
+#Trigger: https://us-west2-silo-systems-292622.cloudfunctions.net/addDeviceToSite?<arguments>
 #input: site_id, device_name, uid
 #output: returns status code 500 if server cannot create new site or 201 on success
 import pymysql
@@ -10,71 +10,76 @@ import sqlalchemy
 from flask import escape
 
 def addDeviceToSite(request):
-    #get arguments to http request
-    request_args = request.args
-    if request_args and 'site_id' in request_args:
-        site_id = request_args['site_id']
-    if request_args and 'device_name' in request_args:
-        device_name = request_args['device_name']
-    if request_args and 'uid' in request_args:
-        uid = request_args['uid']
+   #get arguments to http request
+   request_args = request.args
+   if request_args and 'site_id' in request_args:
+       site_id = request_args['site_id']
+   else: 
+      return ('', 400, {'Access-Control-Allow-Origin':'*'})
+   if request_args and 'device_name' in request_args:
+       device_name = request_args['device_name']
+   else: 
+      return ('', 400, {'Access-Control-Allow-Origin':'*'})
+   if request_args and 'uid' in request_args:
+       uid = request_args['uid']
+   else: 
+      return ('', 400, {'Access-Control-Allow-Origin':'*'})
 
 
-    #connect to the site-user_management database
-    db_user = "root"
-    db_pass = "FbtNb8rkjArEwApg"
-    db_name = "site-user_management"
-    db_socket_dir = "/cloudsql"
-    cloud_sql_connection_name = "silo-systems-292622:us-west1:test-instance"
-    #connect to the site-user_management database
-    pool = sqlalchemy.create_engine(
-        # Equivalent URL:
-        #mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
-        sqlalchemy.engine.url.URL(
-        drivername="mysql+pymysql",
-        username=db_user,  # e.g. "my-database-user"
-        password=db_pass,  # e.g. "my-database-password"
-        database=db_name,  # e.g. "my-database-name"
-        query={
-            "unix_socket": "{}/{}".format(
-                db_socket_dir,  # e.g. "/cloudsql"
-                cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
-            }
-        )
-    )
-    connSiteUserManagement = pool.connect()
-    #execute sql statements
-    #get site's db name & make sure the user is listed as an owner
-    result = connSiteUserManagement.execute(sqlalchemy.text("SELECT db_name FROM site_user_role INNER JOIN site ON site_user_role.site_id = site.site_id where site_user_role.uid = '{}' AND site_user_role.site_id = {} AND site_user_role.role_id = 0;".format(uid, site_id)))
-    if int(result.rowcount) == 0:
-        return('Error: Site does not exist or user does not have ownership permissions', 500, {'Access-Control-Allow-Origin':'*'})
-    r = result.fetchone()
-    db_name = str(r[0])
-    #connect to site's database
-    db_user = "root"
-    db_pass = "FbtNb8rkjArEwApg"
-    db_name = "{}{}".format(db_name)
-    db_socket_dir = "/cloudsql"
-    cloud_sql_connection_name = "silo-systems-292622:us-west1:test-instance"
+   #connect to the site-user_management database
+   db_user = "root"
+   db_pass = "FbtNb8rkjArEwApg"
+   db_name = "site-user_management"
+   db_socket_dir = "/cloudsql"
+   cloud_sql_connection_name = "silo-systems-292622:us-west1:test-instance"
+   #connect to the site-user_management database
+   pool = sqlalchemy.create_engine(
+   # Equivalent URL:
+   #mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
+   sqlalchemy.engine.url.URL(
+   drivername="mysql+pymysql",
+   username=db_user,  # e.g. "my-database-user"
+   password=db_pass,  # e.g. "my-database-password"
+   database=db_name,  # e.g. "my-database-name"
+   query={
+      "unix_socket": "{}/{}".format(
+      db_socket_dir,  # e.g. "/cloudsql"
+      cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
+   })
+   )
+   connSiteUserManagement = pool.connect()
+   #execute sql statements
+   #get site's db name & make sure the user is listed as an owner
+   result = connSiteUserManagement.execute(sqlalchemy.text("SELECT db_name FROM site_user_role INNER JOIN site ON site_user_role.site_id = site.site_id where site_user_role.uid = '{}' AND site_user_role.site_id = {} AND site_user_role.role_id = 0;".format(uid, site_id)))
+   if int(result.rowcount) == 0:
+      return('Error: Site does not exist or user does not have ownership permissions', 500, {'Access-Control-Allow-Origin':'*'})
+   r = result.fetchone()
+   db_name = str(r[0])
+   #connect to site's database
+   db_user = "root"
+   db_pass = "FbtNb8rkjArEwApg"
+   db_name = "{}{}".format(db_name)
+   db_socket_dir = "/cloudsql"
+   cloud_sql_connection_name = "silo-systems-292622:us-west1:test-instance"
 
-    #connect to the site database
-    pool = sqlalchemy.create_engine(
-        # Equivalent URL:
-        #mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
-        sqlalchemy.engine.url.URL(
-        drivername="mysql+pymysql",
-        username=db_user,  # e.g. "my-database-user"
-        password=db_pass,  # e.g. "my-database-password"
-        database=db_name,  # e.g. "my-database-name"
-        query={
-            "unix_socket": "{}/{}".format(
-                db_socket_dir,  # e.g. "/cloudsql"
-                cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
-            }
-        )
-    )
-    connSiteDB = pool.connect()
-    connSiteDB.execute(sqlalchemy.text("CREATE TABLE IF NOT EXISTS `devices`(`device_id` INT NOT NULL AUTO_INCREMENT, `device_name` VARCHAR(25) NOT NULL, PRIMARY KEY(`device_id`));"))
-    connSiteDB.execute(sqlalchemy.text("INSERT INTO devices(device_name) VALUES ('{}');".format(device_name)))
-    return ('', 200, {'Access-Control-Allow-Origin':'*'})
+   #connect to the site database
+   pool = sqlalchemy.create_engine(
+      # Equivalent URL:
+      #mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
+      sqlalchemy.engine.url.URL(
+      drivername="mysql+pymysql",
+      username=db_user,  # e.g. "my-database-user"
+      password=db_pass,  # e.g. "my-database-password"
+      database=db_name,  # e.g. "my-database-name"
+      query={
+         "unix_socket": "{}/{}".format(
+         db_socket_dir,  # e.g. "/cloudsql"
+         cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
+         }
+      )
+   )
+   connSiteDB = pool.connect()
+   connSiteDB.execute(sqlalchemy.text("CREATE TABLE IF NOT EXISTS `devices`(`device_id` INT NOT NULL AUTO_INCREMENT, `device_name` VARCHAR(25) NOT NULL, PRIMARY KEY(`device_id`));"))
+   connSiteDB.execute(sqlalchemy.text("INSERT INTO devices(device_name) VALUES ('{}');".format(device_name)))
+   return ('', 200, {'Access-Control-Allow-Origin':'*'})
     
--- CloudFunctions/removeUserFromSite/main.py
+++ CloudFunctions/removeUserFromSite/main.py
@@ -1,7 +1,7 @@
 #Author(s): Connor Williams
-#Date: 1/3/2021
-#Purpose: Take in arguments from an HTTP request for new_user_email, site_name, and requestor_uid and add the user to the site. 
-# This will allow the user to view data within the site, but they will not be allowed to manage the site. Those privleges are reserved for site owners.
+#Date: 1/22/2021
+#Purpose: Take in arguments from an HTTP request for delete_user_email, site_name, and requestor_uid and delete the user from the site. 
+# This will revoke user privleges to view information about the site
 #Trigger: https://us-west2-silo-systems-292622.cloudfunctions.net/addUserToSite?user_email=user@site.com&site_name=mySite&requestor_uid=abcdabcd
 #input: site_name and uid
 import pymysql
@@ -19,14 +19,10 @@ def addUserToSite(request):
    #get arguments to http request
    request_args = request.args
 
-   if request_args and 'new_user_email' in request_args:
-      new_user_email = request_args['new_user_email']
-      new_user_email.lower()
+   if request_args and 'delete_user_email' in request_args:
+      delete_user_email = request_args['delete_user_email']
+      delete_user_email.lower()
    else: 
-      return ('', 400, {'Access-Control-Allow-Origin':'*'})
-   if request_args and 'role_id' in request_args:
-      new_user_role_id = request_args['role_id']
-   else:
       return ('', 400, {'Access-Control-Allow-Origin':'*'})
    if request_args and 'site_name' in request_args:
       site_id = request_args['site_id']
@@ -36,9 +32,6 @@ def addUserToSite(request):
       requestor_uid = request_args['requestor_uid']
    else:
       return ('', 400, {'Access-Control-Allow_Origin':'*'})
-
-   if new_user_role_id == 0:
-      return ('Error: You can only have 1 owner per site', 500, {'Access-Control-Allow_Origin':'*'})
       
    #connect to the database
    pool = sqlalchemy.create_engine(
@@ -70,17 +63,9 @@ def addUserToSite(request):
          return ("Error: Requesting user is not the owner of this site")
       else:
          #get uid of user to be added
-         result = conn.execute(sqlalchemy.text("SELECT uid from user where email = '{}';".format(new_user_email)))
+         result = conn.execute(sqlalchemy.text("SELECT uid FROM user WHERE email = '{}';".format(delete_user_email)))
          r = result.fetchone()
-         new_user_uid = str(r[0])
+         delete_user_uid = str(r[0])
          #add new user to site
-         conn.execute(sqlalchemy.text("INSERT INTO site_user_role values({}, '{}', {});".format(site_id, new_user_uid, new_user_role_id)))
+         conn.execute(sqlalchemy.text("DELETE FROM site_user_role WHERE site_id = {} AND delete_user_uid;".format(site_id, delete_user_uid)))
          return ('', 200, {'Access-Control-Allow-Origin':'*'})
-
-
-
-
-
-
-   
-
