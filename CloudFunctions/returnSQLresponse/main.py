25c468b2f3498be8d3c2708ac275b3ef4fd69fda
cawndog6 <will5828@vandals.uidaho.edu>
2021-02-14 17:44:01 -0800

    debugging

--- CloudFunctions/returnSQLresponse/main.py
+++ CloudFunctions/returnSQLresponse/main.py
@@ -6,8 +6,8 @@
 import pymysql
 import sqlalchemy
 from flask import escape
-from firebase_admin import auth
-from firebase_admin import credentials
+
+import firebase
 
 def returnSQLresponse(request):
     default_app = firebase_admin.initialize_app()
