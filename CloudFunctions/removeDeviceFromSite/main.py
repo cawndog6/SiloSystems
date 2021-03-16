bfb7e1528270b87963b13bcdb8ca1637a843e2a8
Connor Williams <47013360+cawndog6@users.noreply.github.com>
2021-03-15 19:53:48 -0700

    d

--- CloudFunctions/removeDeviceFromSite/main.py
+++ CloudFunctions/removeDeviceFromSite/main.py
@@ -57,8 +57,8 @@ def removeDeviceFromSite(request):
       database=db_name,  # e.g. "my-database-name"
       query={
          "unix_socket": "{}/{}".format(
-             db_socket_dir,  # e.g. "/cloudsql"
-             cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
+            db_socket_dir,  # e.g. "/cloudsql"
+            cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
       }
    )
    connSiteUserManagement = pool.connect()
